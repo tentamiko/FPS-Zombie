2e69d72c0046db949a8fe41053e8476757f18b16
hunnu029 <shutachimura@gmail.com>
2016-08-25 22:18:33 +0900

    方針の切り替え
    なので色々消した

--- Assets/Script/FPSScript.cs
+++ Assets/Script/FPSScript.cs
@@ -2,12 +2,7 @@
 using System.Collections;
 
 public class FPSScript : MonoBehaviour {
-
-	public GameObject gun;
-	public GameObject dangan;
 	public int zandan = 7; 
-
-	public int walk = 0;
 
 	public bool reload = false;
 	public float reloadstart = 0;
@@ -20,22 +15,9 @@ public class FPSScript : MonoBehaviour {
 	RaycastHit hit;
 	// Update is called once per frame
 	void Update () {
-	
-		UnityEngine.Cursor.visible = false;//マウスカーソル表示
-		UnityEngine.Cursor.lockState = CursorLockMode.Locked;//カーソルを真ん中にロック
-		float horz = Input.GetAxis ("Horizontal");//水平方向入力
-		float vert = Input.GetAxis ("Vertical");//垂直方向入力
-		float mousex = Input.GetAxis ("Mouse X");//X座標
-		float mousey = Input.GetAxis ("Mouse Y");//Y座標
-
-		transform.Rotate(Vector3.up * mousex * 200 * Time.deltaTime);//水平方向回転
-		Camera.main.transform.Rotate(Vector3.left * mousey * 100 * Time.deltaTime);//垂直方向回転
-		//Player.transform.Rotate(Vector3.up * mousey * 100 * Time.deltaTime);
 
 		//attack
 		if(Input.GetMouseButtonDown(0) && zandan >= 1 && reload == false){
-			Instantiate(dangan,new Vector3 (gun.transform.position.x,gun.transform.position.y,gun.transform.position.z),
-				Quaternion.Euler(0,0,0));
 			zandan--;
 		}
 		//reload
---
+++ Assets/Script/PlayerNavScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerNavScript : MonoBehaviour {
+
+	public Transform target;
+	NavMeshAgent agent;
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		agent.SetDestination (target.position);
+	}
+		
+	public void Advance(){
+		agent.SetDestination (target.position);
+	}
+
+}
